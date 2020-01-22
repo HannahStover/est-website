f65b9087e324a9d2b14fd6084ee3419dc6c78117
Hannah Stover <hrosestover@gmail.com>
2020-01-21 22:04:27 -0800

    Added Component files, redux files, and baseUrl

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import Approach from './ApproachComponent';
+import Fees from './FeesComponent';
+import Forms from './FormsComponent';
+import Contact from './ContactComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
